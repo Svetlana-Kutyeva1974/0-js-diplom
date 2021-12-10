899f228f99e391f6645e31b385aebad259272aaf
Svetlana Kutyova <kutyovas@yandex.ru>
2021-12-10 13:54:38 +0300

    3

--- src/js/GamePlay.js
+++ src/js/GamePlay.js
@@ -125,7 +125,6 @@ export default class GamePlay {
    * @param callback
    */
   addNewGameListener(callback) {
-    console.log('играем первый раз');
     this.newGameListeners.push(callback);
   }
 
