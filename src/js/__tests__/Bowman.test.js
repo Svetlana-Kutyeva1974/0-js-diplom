2086737e82a1dd1c21857fdd10249ece5c973e91
Svetlana Kutyova <kutyovas@yandex.ru>
2022-01-29 03:46:31 +0300

    damage

---
+++ src/js/__tests__/Bowman.test.js
@@ -0,0 +1,43 @@
+import Bowman from '../Bowman.js';
+import Character from '../app.js';
+
+/*test('string length name error', () => {
+  const result = new Bowman('hero', 'Bowman');
+  expect(result.attack).toBe(Character.TYPES[`${result.type}`][0]);
+});
+
+test('string length name error', () => {
+  const result0 = new Bowman('hero', 'Bowman');
+  expect(result0.defence).toBe(Character.TYPES[`${result0.type}`][1]);
+});
+
+test('Повышение уровня умершего персонажа. Ошибка', () => {
+  expect(() => {
+    const result = new Bowman('hero', 'Bowman');
+    result.health = 0;
+    result.levelUp();
+  }).toThrow('Нельзя повысить уровень умершего');
+});
+
+test('Повышение уровня персонажа', () => {
+  const result = new Bowman('hero', 'Bowman');
+  result.health = 80;
+  result.levelUp();
+  const expected = {
+    name: 'hero',
+    type: 'Bowman',
+    health: 100,
+    level: 2,
+    attack: 30,
+    defence: 30,
+  };
+  expect(result).toEqual(expected);
+});
+
+test('damage уменьшение здоровья персонажа', () => {
+  const heroes = new Bowman('hero', 'Bowman');
+  heroes.damage(65);
+  const result = heroes.health;
+  expect(result).toBeCloseTo(51.25);
+});
+*/
---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,32 @@
+import Character from '../app.js';
+/*
+test('app string length name error', () => {
+  expect(() => {
+    // eslint-disable-next-line no-unused-vars
+    const obj = new Character('heroismerror', 'Magician');
+  }).toThrow(/Ошибка, имя должно содержать не менее 2 и не более 10 символов/);
+});
+
+test('app type error ', () => {
+  expect(() => {
+    // eslint-disable-next-line no-unused-vars
+    const result3 = new Character('hero5', 'Magiciancheck');
+  }).toThrow(/Ошибка, недопустимый тип элемента/);
+});
+
+test('level health error', () => {
+  expect(() => {
+    const result3 = new Character('hero8', 'Bowman');
+    result3.health = 0;
+    result3.levelUp();
+  }).toThrow(/Нельзя повысить уровень умершего/);
+});
+
+test('level domage error', () => {
+  const result3 = new Character('hero8', 'Bowman');
+  result3.health = -1;
+  expect(() => {
+    result3.damage(2000);
+  }).toThrow(/Ошибка, уровень жизни игрока меньше нуля/);
+});
+*/
